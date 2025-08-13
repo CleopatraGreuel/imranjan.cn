c4dd9569241c94fde3ec7799755578b85ba4a994
Clix <imran924@qq.com>
2025-08-13 18:39:24 +0800

    图片格式更改为webp

--- composables/usePhotography.js
+++ composables/usePhotography.js
@@ -19,70 +19,70 @@ export const usePhotography = () => {
             // orientation: 'portrait' 9:16
             // orientation: 'landscape' 4:3
             {
-              src: '/images/photography/街头风室内/1.JPG',
+              src: '/images/photography/街头风室内/1.webp',
               alt: 'Imran',
               orientation: 'portrait',
               size: 'medium'
             },
             {
-              src: '/images/photography/街头风室内/2.JPG',
+              src: '/images/photography/街头风室内/2.webp',
               alt: 'Imran',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室内/3.JPG',
+              src: '/images/photography/街头风室内/3.webp',
               alt: 'Imran',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室内/4.JPG',
+              src: '/images/photography/街头风室内/4.webp',
               alt: 'Imran',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室内/5.JPG',
+              src: '/images/photography/街头风室内/5.webp',
               alt: 'Nabi',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室内/6.JPG',
+              src: '/images/photography/街头风室内/6.webp',
               alt: 'Imran',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室内/7.JPG',
+              src: '/images/photography/街头风室内/7.webp',
               alt: 'Rifat',
               orientation: 'landscape',
               size: 'large'
             },
             {
-              src: '/images/photography/街头风室内/8.JPG',
+              src: '/images/photography/街头风室内/8.webp',
               alt: 'Jasur',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室内/9.JPG',
+              src: '/images/photography/街头风室内/9.webp',
               alt: 'Jasur',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室内/10.JPG',
+              src: '/images/photography/街头风室内/10.webp',
               alt: 'Dilyar',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室内/11.JPG',
+              src: '/images/photography/街头风室内/11.webp',
               alt: 'Dilyar',
               orientation: 'landscape',
               size: 'large'
             },
             {
-              src: '/images/photography/街头风室内/12.JPG',
+              src: '/images/photography/街头风室内/12.webp',
               alt: 'Nabi',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室内/13.JPG',
+              src: '/images/photography/街头风室内/13.webp',
               alt: 'Imran',
               orientation: 'portrait'
             }
@@ -94,32 +94,32 @@ export const usePhotography = () => {
           description: '室外街头风人像摄影',
           photos: [
             {
-              src: '/images/photography/街头风室外/1.JPG',
+              src: '/images/photography/街头风室外/1.webp',
               alt: 'Jasur',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室外/2.JPG',
+              src: '/images/photography/街头风室外/2.webp',
               alt: 'Dilyar',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室外/3.JPG',
+              src: '/images/photography/街头风室外/3.webp',
               alt: 'Dilyar',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室外/4.JPG',
+              src: '/images/photography/街头风室外/4.webp',
               alt: 'Dilyar',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室外/5.JPG',
+              src: '/images/photography/街头风室外/5.webp',
               alt: 'Dilyar',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/街头风室外/6.JPG',
+              src: '/images/photography/街头风室外/6.webp',
               alt: 'Dilyar',
               orientation: 'portrait'
             }
@@ -131,43 +131,43 @@ export const usePhotography = () => {
           description: '夜幕下的足球场，运动与光影的完美结合',
           photos: [
             {
-              src: '/images/photography/夜晚足球场/1.JPG',
+              src: '/images/photography/夜晚足球场/1.webp',
               alt: '夜晚足球场',
               orientation: 'landscape',
               size: 'large'
             },
             {
-              src: '/images/photography/夜晚足球场/2.JPG',
+              src: '/images/photography/夜晚足球场/2.webp',
               alt: '夜晚足球场',
               orientation: 'landscape',
             },
             {
-              src: '/images/photography/夜晚足球场/3.JPG',
+              src: '/images/photography/夜晚足球场/3.webp',
               alt: '夜晚足球场',
               orientation: 'landscape',
             },
             {
-              src: '/images/photography/夜晚足球场/4.JPG',
+              src: '/images/photography/夜晚足球场/4.webp',
               alt: '夜晚足球场',
               orientation: 'landscape'
             },
             {
-              src: '/images/photography/夜晚足球场/5.JPG',
+              src: '/images/photography/夜晚足球场/5.webp',
               alt: '夜晚足球场',
               orientation: 'landscape'
             },
             {
-              src: '/images/photography/夜晚足球场/6.JPG',
+              src: '/images/photography/夜晚足球场/6.webp',
               alt: '夜晚足球场',
               orientation: 'landscape'
             },
             {
-              src: '/images/photography/夜晚足球场/7.JPG',
+              src: '/images/photography/夜晚足球场/7.webp',
               alt: '夜晚足球场',
               orientation: 'landscape'
             },
             {
-              src: '/images/photography/夜晚足球场/8.JPG',
+              src: '/images/photography/夜晚足球场/8.webp',
               alt: '夜晚足球场',
               orientation: 'landscape',
               size: 'medium'
@@ -180,48 +180,48 @@ export const usePhotography = () => {
           description: '狮城印象，现代都市与自然美景的交融',
           photos: [
             {
-              src: '/images/photography/新加坡/1.JPG',
+              src: '/images/photography/新加坡/1.webp',
               alt: 'The Capitol Kempinski Hotel',
               orientation: 'portrait',
               size: 'large'
             },
             {
-              src: '/images/photography/新加坡/2.png',
+              src: '/images/photography/新加坡/2.webp',
               alt: 'Raffles Marina',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/新加坡/3.png',
+              src: '/images/photography/新加坡/3.webp',
               alt: 'Sentosa',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/新加坡/4.JPG',
+              src: '/images/photography/新加坡/4.webp',
               alt: 'NUS',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/新加坡/5.JPG',
+              src: '/images/photography/新加坡/5.webp',
               alt: 'NUS',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/新加坡/6.JPG',
+              src: '/images/photography/新加坡/6.webp',
               alt: 'NUS',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/新加坡/7.JPG',
+              src: '/images/photography/新加坡/7.webp',
               alt: 'Marina Bay Sands',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/新加坡/8.JPG',
+              src: '/images/photography/新加坡/8.webp',
               alt: 'Marina Bay Sands',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/新加坡/9.png',
+              src: '/images/photography/新加坡/9.webp',
               alt: 'Marina Bay Sands',
               orientation: 'portrait'
             }
@@ -233,96 +233,96 @@ export const usePhotography = () => {
           description: '春夏秋冬，花草树木，曲直之美',
           photos: [
             {
-              src: '/images/photography/校园/1.JPG',
+              src: '/images/photography/校园/1.webp',
               alt: '校园生活',
               orientation: 'landscape',
               size: 'large'
             },
             {
-              src: '/images/photography/校园/2.JPG',
+              src: '/images/photography/校园/2.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/3.JPG',
+              src: '/images/photography/校园/3.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/4.JPG',
+              src: '/images/photography/校园/4.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/5.JPG',
+              src: '/images/photography/校园/5.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/6.JPG',
+              src: '/images/photography/校园/6.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/7.JPG',
+              src: '/images/photography/校园/7.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/8.JPG',
+              src: '/images/photography/校园/8.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             // {
-            //   src: '/images/photography/校园/9.JPG',
+            //   src: '/images/photography/校园/9.webp',
             //   alt: '校园生活',
             //   orientation: 'portrait'
             // },
             {
-              src: '/images/photography/校园/10.JPG',
+              src: '/images/photography/校园/10.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/11.JPG',
+              src: '/images/photography/校园/11.webp',
               alt: '校园生活',
               orientation: 'landscape',
               size: 'large'
             },
             {
-              src: '/images/photography/校园/12.JPG',
+              src: '/images/photography/校园/12.webp',
               alt: '校园生活',
               orientation: 'landscape',
               size: 'large'
             },
             {
-              src: '/images/photography/校园/13.JPG',
+              src: '/images/photography/校园/13.webp',
               alt: '校园生活',
               orientation: 'landscape',
               size: 'large'
             },
             {
-              src: '/images/photography/校园/14.JPG',
+              src: '/images/photography/校园/14.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/15.JPG',
+              src: '/images/photography/校园/15.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/16.JPG',
+              src: '/images/photography/校园/16.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/17.JPG',
+              src: '/images/photography/校园/17.webp',
               alt: '校园生活',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/校园/18.JPG',
+              src: '/images/photography/校园/18.webp',
               alt: '校园生活',
               orientation: 'portrait'
             }
@@ -334,22 +334,22 @@ export const usePhotography = () => {
           description: '传统茶文化的温馨角落，时光沉淀的人文情怀',
           photos: [
             {
-              src: '/images/photography/老茶馆/1.JPG',
+              src: '/images/photography/老茶馆/1.webp',
               alt: '老茶馆',
               orientation: 'portrait',
             },
             {
-              src: '/images/photography/老茶馆/2.JPG',
+              src: '/images/photography/老茶馆/2.webp',
               alt: '老茶馆',
               orientation: 'portrait',
             },
             {
-              src: '/images/photography/老茶馆/3.JPG',
+              src: '/images/photography/老茶馆/3.webp',
               alt: '老茶馆',
               orientation: 'portrait',
             },
             {
-              src: '/images/photography/老茶馆/4.JPG',
+              src: '/images/photography/老茶馆/4.webp',
               alt: '老茶馆',
               orientation: 'portrait',
             }
@@ -361,24 +361,24 @@ export const usePhotography = () => {
           description: '山城重庆与长江的壮美交融，城市风光与自然人文的完美结合',
           photos: [
             {
-              src: '/images/photography/重庆/1.JPG',
+              src: '/images/photography/重庆/1.webp',
               alt: '城市',
               orientation: 'landscape',
             },
             {
-              src: '/images/photography/重庆/2.JPG',
+              src: '/images/photography/重庆/2.webp',
               alt: '千厮门大桥',
               orientation: 'landscape',
               size: 'medium'
             },
             {
-              src: '/images/photography/重庆/3.JPG',
+              src: '/images/photography/重庆/3.webp',
               alt: '千厮门大桥',
               orientation: 'landscape',
               size: 'medium'
             },
             {
-              src: '/images/photography/重庆/4.png',
+              src: '/images/photography/重庆/4.webp',
               alt: '长江',
               orientation: 'landscape'
             },
@@ -389,14 +389,14 @@ export const usePhotography = () => {
               size: 'large'
             },
             {
-              src: '/images/photography/重庆/6.png',
+              src: '/images/photography/重庆/6.webp',
               alt: '重庆城景',
               orientation: 'landscape',
               size: 'medium',
               objectPosition: 'center'
             },
             {
-              src: '/images/photography/重庆/7.png',
+              src: '/images/photography/重庆/7.webp',
               alt: '重庆城景',
               orientation: 'landscape',
               size: 'medium',
@@ -410,32 +410,32 @@ export const usePhotography = () => {
           description: '海口风光，椰风海韵，热带风情',
           photos: [
             {
-              src: '/images/photography/海口/1.JPG',
+              src: '/images/photography/海口/1.webp',
               alt: '海口风光',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/海口/2.JPG',
+              src: '/images/photography/海口/2.webp',
               alt: '海口风光',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/海口/3.JPG',
+              src: '/images/photography/海口/3.webp',
               alt: '海口风光',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/海口/4.JPG',
+              src: '/images/photography/海口/4.webp',
               alt: '海口风光',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/海口/5.JPG',
+              src: '/images/photography/海口/5.webp',
               alt: '海口风光',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/海口/6.png',
+              src: '/images/photography/海口/6.webp',
               alt: '海口风光',
               orientation: 'portrait'
             }
@@ -447,32 +447,32 @@ export const usePhotography = () => {
           description: '18:45',
           photos: [
             {
-              src: '/images/photography/火烧云/1.JPG',
+              src: '/images/photography/火烧云/1.webp',
               alt: '晚霞',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/火烧云/2.JPG',
+              src: '/images/photography/火烧云/2.webp',
               alt: '晚霞',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/火烧云/3.JPG',
+              src: '/images/photography/火烧云/3.webp',
               alt: '晚霞',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/火烧云/4.JPG',
+              src: '/images/photography/火烧云/4.webp',
               alt: '晚霞',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/火烧云/5.JPG',
+              src: '/images/photography/火烧云/5.webp',
               alt: '晚霞',
               orientation: 'portrait'
             },
             {
-              src: '/images/photography/火烧云/6.JPG',
+              src: '/images/photography/火烧云/6.webp',
               alt: '火烧云',
               orientation: 'portrait'
             }
